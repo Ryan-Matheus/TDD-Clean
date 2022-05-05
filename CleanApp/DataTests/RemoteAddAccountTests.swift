e335e0160175855908e02174793b94471609ab7c
Ryan Matheus <t3651512@timbrasil.com.br>
2022-05-05 17:27:14 -0300

    move test helper to class extension

--- CleanApp/DataTests/RemoteAddAccountTests.swift
+++ CleanApp/DataTests/RemoteAddAccountTests.swift
@@ -33,6 +33,10 @@ class RemoteAddAccountTests: XCTestCase {
         XCTAssertEqual(httpClientSpy.url, url)
     }
 
+    
+}
+
+extension RemoteAddAccountTests {
     class HttpClientSpy: HttpPostClient {
         var url: URL?
         
