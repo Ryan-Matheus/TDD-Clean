01c1fa9fa20219ee62f65e559e994ef9367cdfd3
Ryan Matheus <t3651512@timbrasil.com.br>
2022-05-06 18:13:22 -0300

    ensure RemoteAddAccount completes with error when HttpClient completes with error

---
+++ CleanApp/Data/Http/HttpError.swift
@@ -0,0 +1,12 @@
+//
+//  HttpError.swift
+//  Data
+//
+//  Created by user on 06/05/22.
+//
+
+import Foundation
+
+public enum HttpError: Error {
+    case noConnectivity
+}
---
+++ CleanApp/Domain/Errors/DomainErrors.swift
@@ -0,0 +1,12 @@
+//
+//  DomainErrors.swift
+//  Domain
+//
+//  Created by user on 06/05/22.
+//
+
+import Foundation
+
+public enum DomainError: Error {
+    case unexpected
+}
