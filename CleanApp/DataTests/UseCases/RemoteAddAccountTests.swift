64fe0dd1abae89f1590768618b3cf40d82a77573
Ryan Matheus <t3651512@timbrasil.com.br>
2022-05-12 17:51:18 -0300

    simplify code with helpers

--- CleanApp/DataTests/UseCases/RemoteAddAccountTests.swift
+++ CleanApp/DataTests/UseCases/RemoteAddAccountTests.swift
@@ -11,7 +11,7 @@ import Data
 
 class RemoteAddAccountTests: XCTestCase {
     func test_add_shoud_call_httpClient_with_correct_url() {
-        let url = URL(string: "http://any-url.com")!
+        let url = makeUrl()
         let (sut, httpClientSpy) = makeSut(url: url)
         sut.add(addAccountModel: makeAddAccountModel()) { _ in }
         XCTAssertEqual(httpClientSpy.urls, [url])
@@ -42,7 +42,7 @@ class RemoteAddAccountTests: XCTestCase {
     
     func test_add_shoud_complete_with_error_if_client_completes_invalid_data() {
         let (sut, httpClientSpy) = makeSut()
-        expect(sut, completeWith: .failure(.unexpected), when: { httpClientSpy.completeWithData(Data("invalid_data".utf8))
+        expect(sut, completeWith: .failure(.unexpected), when: { httpClientSpy.completeWithData(Data(makeInvalidData()))
         })
     }
 
@@ -70,6 +70,14 @@ extension RemoteAddAccountTests {
         }
         action()
         wait(for: [exp], timeout: 1)
+    }
+    
+    func makeInvalidData() -> Data {
+        return Data("invalid_data".utf8)
+    }
+    
+    func makeUrl() -> URL {
+        return URL(string: "http://any-url.com")!
     }
     
     func makeAddAccountModel() -> AddAccountModel {
