d19097df7bd696ff3bb4434faa6aa97ffb3d5b03
Ryan Matheus <t3651512@timbrasil.com.br>
2022-05-18 17:28:36 -0300

    check memory leak for sut

--- CleanApp/infraTests/AlamoFireAdapterTests.swift
+++ CleanApp/infraTests/AlamoFireAdapterTests.swift
@@ -39,11 +39,13 @@ class AlamoFireAdapterTests: XCTestCase {
 }
 
 extension AlamoFireAdapterTests {
-    func makeSut() -> AlamoFireAdapter {
+    func makeSut(file: StaticString = #file, line: UInt = #line) -> AlamoFireAdapter {
         let configuration = URLSessionConfiguration.default
         configuration.protocolClasses = [UrlProtocolStub.self]
         let session = Session(configuration: configuration)
-        return AlamoFireAdapter(session: session)
+        let sut = AlamoFireAdapter(session: session)
+        checkMemoryLeak(for: sut, file: file, line: line)
+        return sut
     }
     
     func testRequestFor(url: URL = makeUrl(), data: Data?, action: @escaping (URLRequest) -> Void) {
