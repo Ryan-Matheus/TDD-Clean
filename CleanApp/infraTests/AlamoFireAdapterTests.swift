b268a25906b13843441f01d8d53d7a6559fb0a7b
Ryan Matheus <t3651512@timbrasil.com.br>
2022-05-18 15:45:10 -0300

    ensure Alamofire Adapter calls Alamofire with correct data

--- CleanApp/infraTests/AlamoFireAdapterTests.swift
+++ CleanApp/infraTests/AlamoFireAdapterTests.swift
@@ -15,8 +15,9 @@ class AlamoFireAdapter {
         self.session = session
     }
     
-    func post(to url: URL) {
-        session.request(url, method: .post).resume()
+    func post(to url: URL, with data: Data?) {
+        let json = try? JSONSerialization.jsonObject(with: data!, options: .allowFragments) as? [String: Any]
+        session.request(url, method: .post, parameters: json, encoding: JSONEncoding.default).resume()
     }
 }
 
@@ -27,10 +28,11 @@ class AlamoFireAdapterTests: XCTestCase {
         configuration.protocolClasses = [UrlProtocolStub.self]
         let session = Session(configuration: configuration)
         let sut = AlamoFireAdapter(session: session)
-        sut.post(to: url)
+        sut.post(to: url, with: makeValidData())
         let exp = expectation(description: "waiting")
         UrlProtocolStub.observerRequest { request in
             XCTAssertEqual("POST", request.httpMethod)
+            XCTAssertNotNil(request.httpBodyStream)
             exp.fulfill()
         }
         wait(for: [exp], timeout: 1)
