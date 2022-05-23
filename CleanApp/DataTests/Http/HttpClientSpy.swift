64b47c6951072e3ace0c8bbd1e9be29d1aece07d
Ryan Matheus <t3651512@timbrasil.com.br>
2022-05-23 16:50:38 -0300

    urlProtocolStub bugs solved

--- CleanApp/DataTests/Http/HttpClientSpy.swift
+++ CleanApp/DataTests/Http/HttpClientSpy.swift
@@ -7,17 +7,20 @@
 
 import Foundation
 import Data
+import infra
 
 class HttpClientSpy: HttpPostClient {
+    func post(to url: URL, with data: Data?, completion: @escaping (Result<Data?, HttpError>) -> Void) {
+        self.urls.append(url)
+        self.data = data
+        self.completion = completion
+
+    }
+    
     var urls = [URL]()
     var data: Data?
     var completion: ((Result<Data?, HttpError>) -> Void)?
     
-    func post(to url: URL, with data: Data?, completion: @escaping (Result<Data, HttpError>) -> Void) {
-        self.urls.append(url)
-        self.data = data
-        self.completion = completion
-    }
     func completeWithError(_ error: HttpError) {
         completion?(.failure(error))
     }
