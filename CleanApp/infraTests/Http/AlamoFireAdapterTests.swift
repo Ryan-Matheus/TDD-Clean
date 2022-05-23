747d4aa51b6f08bd6fd4bd2b25b8bf481a844d5b
Ryan Matheus <t3651512@timbrasil.com.br>
2022-05-23 16:08:25 -0300

    move UrlProtocolStub to it own file

--- CleanApp/infraTests/Http/AlamoFireAdapterTests.swift
+++ CleanApp/infraTests/Http/AlamoFireAdapterTests.swift
@@ -3,42 +3,6 @@ import Alamofire
 import Data
 import infra
 
-class AlamoFireAdapter: HttpPostClient {
-    private let session: Session
-    
-    init(session: Session = .default) {
-        self.session = session
-    }
-    
-    func post(to url: URL, with data: Data?, completion: @escaping (Result<Data?, HttpError>) -> Void) {
-        session.request(url, method: .post, parameters: data?.toJson(), encoding: JSONEncoding.default).responseData
-        { dataResponse in
-            guard let statusCode = dataResponse.response?.statusCode else {
-                return completion(.failure(.noConnectivity)) }
-            switch dataResponse.result {
-            case .failure: completion(.failure(.noConnectivity))
-            case .success(let data):
-                switch statusCode {
-                case 204:
-                    completion(.success(nil))
-                case 200...299:
-                    completion(.success(data))
-                case 401:
-                    completion(.failure(.unauthorized))
-                case 403:
-                    completion(.failure(.forbidden))
-                case 400...499:
-                    completion(.failure(.badRequest))
-                case 500...599:
-                    completion(.failure(.serverError))
-                default:
-                    completion(.success(data))
-                }
-            }
-        }
-    }
-}
-
 class AlamoFireAdapterTests: XCTestCase {
     func test_post_should_make_request_with_valid_url_and_method() {
         let url = makeUrl()
@@ -125,47 +89,5 @@ extension AlamoFireAdapterTests {
         }
         wait(for: [exp], timeout: 1)
     }
-}
-
-class UrlProtocolStub: URLProtocol {
-    static var emit: ((URLRequest) -> Void)?
-    static var data: Data?
-    static var response: HTTPURLResponse?
-    static var error: Error?
-    
-    
-    static func observerRequest(completion: @escaping (URLRequest) -> Void) {
-        UrlProtocolStub.emit = completion
-    }
-    
-    static func simulate(data: Data?, response: HTTPURLResponse?, error: Error?) {
-        UrlProtocolStub.data = data
-        UrlProtocolStub.response = response
-        UrlProtocolStub.error = error
-    }
-    
-   override open class func canInit(with request: URLRequest) -> Bool {
-        return true
-    }
-    
-    override open class func canonicalRequest(for request: URLRequest) -> URLRequest {
-        return request
-    }
-    
-    override open func startLoading() {
-        UrlProtocolStub.emit?(request)
-        if let data = UrlProtocolStub.data {
-            client?.urlProtocol(self, didLoad: data)
-        }
-        if let response = UrlProtocolStub.response {
-            client?.urlProtocol(self, didReceive: response, cacheStoragePolicy: .notAllowed)
-        }
-        if let error = UrlProtocolStub.error {
-            client?.urlProtocol(self, didFailWithError: error)
-        }
-        client?.urlProtocolDidFinishLoading(self)
-    }
-
-    override open func stopLoading() {}
 }
 
